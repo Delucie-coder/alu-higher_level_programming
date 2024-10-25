98a8a3997bda2b97abbdf677201389707ad1a6d0
Delucie_coder <d.rurangwa@alustudent.com>
2024-10-25 11:53:28 -0700

    list integer

--- python-data-structures/0-print_list_integer.py
+++ python-data-structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for elements in my_list:
-        print('{:d}'.format(elements))
+def square_matrix_simple(matrix=[]):
+    if not matrix:
+        return []
+    return [[x ** 2 for x in row] for row in matrix]
