5566f8b8e4c41d028b0489fb3cb0978d74bf4cf6
Delucie_coder <d.rurangwa@alustudent.com>
2024-10-25 13:18:17 -0700

    no

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        if not row:
-            print()
-        else:
-            for num in row:
-                if num == row[-1]:
-                    print('{:d}'.format(num))
-                else:
-                    print('{:d}'.format(num), end=" ")
+def no_c(my_string):
+    my_string = my_string.translate({ord("c"): None})
+    my_string = my_string.translate({ord("C"): None})
+    return my_string
