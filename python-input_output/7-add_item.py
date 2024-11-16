b40a7cb5fefd91a7212980f10b15df013a70a46f
Delucie-coder <d.rurangwa@alustudent.com>
2024-11-16 21:22:29 +0200

    Update 7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Script that adds all arguments to a Python list and saves them to a file"""
+"""adds all arguments to a Python list, and then save them to a file:"""
+
+
 import sys
-from os import path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-if path.exists(filename):
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file') \
+        .load_from_json_file
 
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, filename)
+    try:
+        loadFile = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        loadFile = []
+
+    argc = len(sys.argv)
+    for idx in range(1, argc):
+        loadFile.append(sys.argv[idx])
+    save_to_json_file(loadFile, "add_item.json")
