562a1d622023da58e6ef8473179a88c2d4857f1d
Delucie_coder <d.rurangwa@alustudent.com>
2024-11-30 15:39:39 -0800

    Task

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script to send a GET request and display the body of the r
+# Bash script that takes in a URL, sends a 'GET' request to the URL, and displays the body of the response
 curl -sL "$1"
