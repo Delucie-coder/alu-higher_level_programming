97b92d3a4aa54ecabb02e46f97f912d7fda9184b
Delucie-coder <d.rurangwa@alustudent.com>
2024-11-30 18:43:01 +0000

    update

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
-#  Bash script that takes in a URL, sends a 'POST' request to the passed URL, and displays the body of the response
-curl -sX POST -d email=test@gmail.com -d subject="I will always be here for PLD" "$1"
+ends a GET request to a URL and displays the body for 200 status code
+curl -sL "$1"
