71fc40c12a176d0c74522ae99c882452b89b84a0
Delucie_coder <d.rurangwa@alustudent.com>
2024-11-30 14:42:49 -0800

    update

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to send a GET request and display the body of the r
-curl -sL -w "%{http_code}" "$1" -o response.txt | grep -q "200" && cat response.txt
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1"
